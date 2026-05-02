06cfb81ff3abb8c4521b61c53f5f36f13a03907a
rileycombs <rileytcombs@gmail.com>
2026-05-02 02:42:37 +0000

    Fri, May 1, 2026, 10:42 PM -04:00

---
+++ FP.py
@@ -0,0 +1,427 @@
+# cs32-final-project
+#CS32 Final Project with Leah and Riley
+characters = [
+    {
+        "name": "Michael Scott",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "management",
+        "department_detail": "scranton_management",
+        "has_catchphrase": True,
+        "season_introduced": 1
+    },
+    {
+        "name": "Dwight Schrute",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": True,
+        "has_office_romance": True,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": True,
+        "season_introduced": 1
+    },
+    {
+        "name": "Jim Halpert",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Pam Beesly",
+        "gender": "female",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "reception",
+        "department_detail": "scranton_front_desk",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Angela Martin",
+        "gender": "female",
+        "hair": "blonde",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "accounting",
+        "department_detail": "scranton_accounting",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Kevin Malone",
+        "gender": "male",
+        "hair": "bald",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "accounting",
+        "department_detail": "scranton_accounting",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Oscar Martinez",
+        "gender": "male",
+        "hair": "black",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "accounting",
+        "department_detail": "scranton_accounting",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Stanley Hudson",
+        "gender": "male",
+        "hair": "black",
+        "glasses": True,
+        "has_office_romance": False,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Phyllis Vance",
+        "gender": "female",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Andy Bernard",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": True,
+        "season_introduced": 3
+    },
+    {
+        "name": "Ryan Howard",
+        "gender": "male",
+        "hair": "black",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "temp",
+        "department_detail": "scranton_temp",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Kelly Kapoor",
+        "gender": "female",
+        "hair": "black",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "customer_service",
+        "department_detail": "scranton_customer_service",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Toby Flenderson",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "hr",
+        "department_detail": "scranton_hr",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Creed Bratton",
+        "gender": "male",
+        "hair": "gray",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "quality_assurance",
+        "department_detail": "scranton_quality_assurance",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Meredith Palmer",
+        "gender": "female",
+        "hair": "red",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "supplier_relations",
+        "department_detail": "scranton_supplier_relations",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Darryl Philbin",
+        "gender": "male",
+        "hair": "black",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "warehouse",
+        "department_detail": "warehouse",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Jan Levinson",
+        "gender": "female",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "corporate",
+        "department_detail": "corporate",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    },
+    {
+        "name": "Holly Flax",
+        "gender": "female",
+        "hair": "blonde",
+        "glasses": True,
+        "has_office_romance": True,
+        "role": "hr",
+        "department_detail": "scranton_hr",
+        "has_catchphrase": False,
+        "season_introduced": 4
+    },
+    {
+        "name": "David Wallace",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "corporate",
+        "department_detail": "corporate",
+        "has_catchphrase": False,
+        "season_introduced": 2
+    },
+    {
+        "name": "Robert California",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "management",
+        "department_detail": "scranton_management",
+        "has_catchphrase": False,
+        "season_introduced": 8
+    },
+    {
+        "name": "Gabe Lewis",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "corporate",
+        "department_detail": "corporate",
+        "has_catchphrase": False,
+        "season_introduced": 6
+    },
+    {
+        "name": "Erin Hannon",
+        "gender": "female",
+        "hair": "red",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "reception",
+        "department_detail": "scranton_front_desk",
+        "has_catchphrase": False,
+        "season_introduced": 5
+    },
+    {
+        "name": "Nellie Bertram",
+        "gender": "female",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": False,
+        "role": "management",
+        "department_detail": "scranton_management",
+        "has_catchphrase": False,
+        "season_introduced": 8
+    },
+    {
+        "name": "Karen Filippelli",
+        "gender": "female",
+        "hair": "black",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "sales",
+        "department_detail": "scranton_sales",
+        "has_catchphrase": False,
+        "season_introduced": 3
+    },
+    {
+        "name": "Roy Anderson",
+        "gender": "male",
+        "hair": "brown",
+        "glasses": False,
+        "has_office_romance": True,
+        "role": "warehouse",
+        "department_detail": "warehouse",
+        "has_catchphrase": False,
+        "season_introduced": 1
+    }
+]
+
+# Funny final boss questions
+final_questions = {
+    "Michael Scott": "Did your character start a paper company out of spite?",
+    "Dwight Schrute": "Does your character kill his secret coworker girlfriend's cat? (He was putting it out of its misery, okay?)",
+    "Jim Halpert": "Did your character move to a different city to not have to see his crush get married?",
+    "Pam Beesly": "Did your character's boss sleep with their mom?'",
+    "Angela Martin": "Did your character enjoy watching two male coworkers fight for her love in the parking lot?",
+    "Kevin Malone": "Did your character spill a giant pot of chili? (We are still crying)",
+    "Oscar Martinez": "Did your character have a secret love affair with their coworker's husband (also a state senator!)?",
+    "Stanley Hudson": "Did your character stutter?",
+    "Phyllis Vance": "Is your character married to Bob Vance of Vance Refrigeration?",
+    "Andy Bernard": "Did your character get a butt tattoo to prove a point?",
+    "Ryan Howard": "Did your character commit wire fraud, get arrested, get fired, and beg for another job at the company?",
+    "Kelly Kapoor": "Does your character look so good in white that she has no choice but to wear it to her coworker's wedding?",
+    "Toby Flenderson": "Does your character jump a fence and runaway to Costa Rica? (Everything is always his fault)",
+    "Creed Bratton": "Is this character the author of the blog creedthoughts.gov.www (it's just a word doc, but he doesn't need to know that)",
+    "Meredith Palmer": "Did your character shave their head after learning they had lice?",
+    "Darryl Philbin": "Did your character teach their boss fake street slang?",
+    "Jan Levinson": "Was your character fired from Dunder Mifflin for erratic behavior and impulsive breast augmentation (because 'Michael likes them')?",
+    "Holly Flax": "Is your character Michael Scott's soup snake?",
+    "David Wallace": "Does your character begrudgingly believe in Michael?",
+    "Robert California": "Is your character the lizard king?",
+    "Gabe Lewis": "Is hugging your character like hugging a skeleton?",
+    "Erin Hannon": "Did your character reunite with her biologoical parents on the show?",
+    "Nellie Bertram": "Did your character randomly take Andy's manager job?",
+    "Karen Filippelli": "Did your character unforunately third wheel in their own relationship?",
+    "Roy Anderson": "Does your character need anger management classes?"
+}
+
+# All possible characteristics we can ask about
+question_bank = [
+    ("gender", "Is your character male? (yes/no): ", "male"),
+    ("glasses", "Does your character wear glasses? (yes/no): ", True),
+    ("has_office_romance", "Does your character have an office romance? (yes/no): ", True),
+    ("hair", "Does your character have brown hair? (yes/no): ", "brown"),
+    ("role", "Does your character work in sales? (yes/no): ", "sales"),
+    ("role", "Does your character work in management? (yes/no): ", "management"),
+    ("role", "Does your character work in corporate? (yes/no): ", "corporate"),
+    ("has_catchphrase", "Does your character have a catchphrase? (yes/no): ", True),
+
+    ("department_detail", "Do they work in accounting? (yes/no): ", "scranton_accounting"),
+    ("department_detail", "Do they work in HR? (yes/no): ", "scranton_hr"),
+    ("department_detail", "Do they work in customer service? (yes/no): ", "scranton_customer_service"),
+    ("department_detail", "Do they work in the warehouse? (yes/no): ", "warehouse"),
+    ("department_detail", "Do they work at the front desk? (yes/no): ", "scranton_front_desk"),
+
+    ("season_introduced", "Were they introduced in season 1? (yes/no): ", 1),
+    ("season_introduced", "Were they introduced in season 3? (yes/no): ", 3),
+    ("season_introduced", "Were they introduced in season 4? (yes/no): ", 4),
+    ("season_introduced", "Were they introduced after season 5? (yes/no): ", 8),
+]
+
+# Filtering function
+def filter_characters(characters, attribute, value):
+    return [c for c in characters if c[attribute] == value]
+
+# Best questions function
+def choose_best_question(remaining, used_questions):
+    best_question = None
+    best_score = -1
+
+    for question in question_bank:
+
+        if question in used_questions:
+            continue
+
+        attr, question_text, yes_value = question
+
+        yes_count = 0
+        no_count = 0
+
+        for c in remaining:
+            if c[attr] == yes_value:
+                yes_count += 1
+            else:
+                no_count += 1
+
+        # better score if split is closer to half and half
+        score = min(yes_count, no_count)
+
+        if score > best_score:
+            best_score = score
+            best_question = question
+
+    return best_question
+
+# Game function
+def play_game(characters):
+    remaining = characters.copy()
+    used_questions = set()
+
+    print("\nThink of a character from The Office!")
+
+    while len(remaining) > 1:
+
+        question = choose_best_question(remaining, used_questions)
+
+        if not question:
+            break
+
+        attr, question_text, yes_value = question
+
+        used_questions.add(question)
+
+        answer = input(question_text).strip().lower()
+
+        if answer == "yes":
+            remaining = filter_characters(remaining, attr, yes_value)
+        elif answer == "no":
+            remaining = [c for c in remaining if c[attr] != yes_value]
+        else:
+            print("Please answer yes or no.")
+            continue
+
+        # Show remaining possibilities
+        print("\nRemaining characters:")
+        for c in remaining:
+            print("-", c["name"])
+        print()
+
+    # Final result
+    if len(remaining) == 1:
+
+        final_character = remaining[0]["name"]
+
+        print("\nI know EXACTLY who this is.")
+
+        # Ask funny final boss question
+        if final_character in final_questions:
+            input(final_questions[final_character] + " (press enter) ")
+
+        print(f"\nYour character is {final_character}!")
+
+    elif len(remaining) == 0:
+        print("No matching character found. Were your answers consistent?")
+    else:
+        print("I'm not completely sure. Possible characters:")
+        for c in remaining:
+            print("-", c["name"])
+
+# Replay loop
+while True:
+    play_game(characters)
+    again = input("\nPlay again? (yes/no): ").strip().lower()
+    if again != "yes":
+        print("Thanks for playing!")
+        break
